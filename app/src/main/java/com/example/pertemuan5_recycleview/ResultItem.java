80417b6fcbe392d496c6929cc926e7614e8bfaae
aininnizar <th3.anpn@gmail.com>
2022-12-13 12:52:18 +0700

    RestApi

---
+++ app/src/main/java/com/example/pertemuan5_recycleview/ResultItem.java
@@ -0,0 +1,12 @@
+package com.example.pertemuan5_recycleview;
+
+public class ResultItem {
+    private String name;
+    private String url;
+
+    public String getName(){
+        return name;    }
+    public String getUrl(){
+        return url;
+    }
+}
